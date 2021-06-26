6f0f5ae15c3337702a00813dafddadd4f389c936
montana <montana0101@outlook.com>
2021-06-26 19:32:08 +0800

    初始化

--- src/main/java/com/montana/wiki/config/WikiApplication.java
+++ src/main/java/com/montana/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.montana.wiki;
+package com.montana.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.montana")
 @SpringBootApplication
 public class WikiApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
